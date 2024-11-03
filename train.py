c7a8cfd09f040ce9bd9bb71e564cc4beb11ab4ac
LucasChan <chanconan@gmail.com>
2024-11-03 15:05:16 +0800

    trian script

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import os
 from dataclasses import dataclass, field
 from typing import Optional
+import logging
+from datetime import datetime
 
 import torch
 import transformers
@@ -13,6 +15,12 @@ from loss import ContrastiveLoss
 from trainer import ContrastiveSTTrainer
 from utils import get_available_gpu_idx
 
+CURRENT_TIME = datetime.now().strftime("%b-%d_%H-%M")
+
+logger = logging.getLogger("training")
+logging.basicConfig(
+    filename="logs/training/{}.log".format(CURRENT_TIME), filemode="w", level=logging.INFO
+)
 
 @dataclass
 class ModelArguments:
@@ -58,14 +66,6 @@ class ModelArguments:
             "help": "The specific model version to use (can be a branch name, tag name or commit id)."
         },
     )
-    use_auth_token: bool = field(
-        default=False,
-        metadata={
-            "help": "Will use the token generated when running `transformers-cli login` (necessary to use this script "
-            "with private models)."
-        },
-    )
-
 
 @dataclass
 class DataArguments:
@@ -74,10 +74,6 @@ class DataArguments:
     """
 
     # Huggingface's original arguments.
-    dataset_name: Optional[str] = field(
-        default=None,
-        metadata={"help": "The name of the dataset to use (via the datasets library)."},
-    )
     dataset_config_name: Optional[str] = field(
         default=None,
         metadata={
@@ -88,68 +84,89 @@ class DataArguments:
         default=False,
         metadata={"help": "Overwrite the cached training and evaluation sets"},
     )
-    validation_split_percentage: Optional[int] = field(
-        default=5,
-        metadata={
-            "help": "The percentage of the train set used as validation set in case there's no validation split"
-        },
-    )
     preprocessing_num_workers: Optional[int] = field(
         default=None,
         metadata={"help": "The number of processes to use for the preprocessing."},
     )
-
     train_file: Optional[str] = field(
         default=None, metadata={"help": "The training data file (.txt or .csv)."}
     )
 
+@dataclass
+class ContrastiveSTTrainingArguments(SentenceTransformerTrainingArguments):
+    pass
+
 
 def main():
-    available_gpu_idx = get_available_gpu_idx()
-    if available_gpu_idx is None:
-        raise ValueError("No available GPU found!")
+    # available_gpu_idx = get_available_gpu_idx()
+    # if available_gpu_idx is None:
+    #     raise ValueError("No available GPU found!")
 
-    available_cuda = f"cuda:{available_gpu_idx}"
-    print(f"Using GPU: {available_cuda}")
+    # available_cuda = f"cuda:{available_gpu_idx}"
+    # print(f"Using GPU: {available_cuda}")
     
-    device = torch.device(available_cuda)
+    # device = torch.device(available_cuda)
     
-    parser = HfArgumentParser((ModelArguments, DataArguments))
-    model_args, data_args = parser.parse_args_into_dataclasses()
+    parser = HfArgumentParser((ModelArguments, DataArguments, ContrastiveSTTrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
     
     model = SentenceTransformer(model_args.model_name_or_path)
     
-    set_seed(42)
+    set_seed(training_args.seed)
+    
+    print(training_args)
 
-    training_args = SentenceTransformerTrainingArguments(
-        output_dir="output",
-        overwrite_output_dir=True,
-        num_train_epochs=3,
-        per_device_train_batch_size=16,
-        save_steps=1000,
-        save_total_limit=2,
-        remove_unused_columns=False,
-        learning_rate=2e-5,
-        adam_beta1=0.9,
-        adam_beta2=0.999,
-        adam_epsilon=1e-6,
-        weight_decay=0.01,
-        warmup_steps=100,
-        logging_dir="log",
-        logging_steps=10,
-    )
+    # training_args = SentenceTransformerTrainingArguments(
+    #     output_dir="output",
+    #     overwrite_output_dir=True,
+    #     num_train_epochs=3,
+    #     per_device_train_batch_size=16,
+    #     save_steps=1000,
+    #     save_total_limit=2,
+    #     remove_unused_columns=False,
+    #     learning_rate=2e-5,
+    #     adam_beta1=0.9,
+    #     adam_beta2=0.999,
+    #     adam_epsilon=1e-6,
+    #     weight_decay=0.01,
+    #     warmup_steps=100,
+    #     logging_dir="log",
+    #     logging_steps=10,
+    # )
 
     train_dataset = load_dataset("csv", data_files=data_args.train_file)
+    
+    print(train_dataset)
 
-    loss = ContrastiveLoss(device=device)
+    # loss = ContrastiveLoss(device=device)
 
-    trainer = ContrastiveSTTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset,
-        loss=loss,
-    )
-    trainer.train()
+    # trainer = ContrastiveSTTrainer(
+    #     model=model,
+    #     args=training_args,
+    #     train_dataset=train_dataset,
+    #     loss=loss,
+    # )
+    
+    # # Train
+    # checkpoint = None
+    # if training_args.resume_from_checkpoint is not None:
+    #     checkpoint = training_args.resume_from_checkpoint
+        
+    # train_result = trainer.train(resume_from_checkpoint=checkpoint)
+    # trainer.save_model(training_args.output_dir)
+    
+    # output_train_file = os.path.join(training_args.output_dir, "train_results_{}.txt".format(CURRENT_TIME))
+    
+    # if trainer.is_world_process_zero():
+    #     with open(output_train_file, "w") as writer:
+    #         logger.info("***** Train results *****")
+            
+    #         for key, value in sorted(train_result.metrics.items()):
+    #             logger.info(f"  {key} = {value}")
+    #             writer.write(f"{key} = {value}\n")
+        
+    #     trainer.state.save_to_json(os.path.join(training_args.output_dir, "trainer_state_{}.json".format(CURRENT_TIME)))
+    
 
 if __name__ == "__main__":
     main()
--- train.sh
+++ train.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+formatted_date=$(date +'%b-%d_%H-%M')
+echo "The formatted date is: $formatted_date"
+
 python3 train.py \
-    --model_name_or_path=sentence-transformers/all-mpnet-base-v2 \
-    --train_file=data/keywords.csv \
+    --output_dir output/training/$formatted_time/ \
+    --model_name_or_path sentence-transformers/all-mpnet-base-v2 \
+    --train_file data/keywords.csv \
+    --logging_dir logs/training/$formatted_time/ \
+    --logging_steps=10 \
+    --overwrite_output_dir \
+    --seed 1016 \
+    --bf16 \
+    --num_train_epochs 3 \
+    --save_strategy steps \
+    --save_steps 500 \
+    
