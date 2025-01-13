d2abfad66bd62c95ec8971b9529dabbe8ffc45c9
LucasChan <chanconan@gmail.com>
2025-01-13 16:38:24 +0800

    move get_emb_sim

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import re
 
 import nvidia_smi
 import torch
+from sentence_transformers import SentenceTransformer
 
 
 def print_json_error(text: str, error_msg: str) -> None:
@@ -73,3 +74,38 @@ def get_available_gpu_idx():
 
     # If no available GPU is found
     return None
+
+
+def get_emb_sim(model, source, target):
+    source_emb = model.encode(source, convert_to_tensor=True)
+    target_emb = model.encode(target, convert_to_tensor=True)
+
+    sim = torch.cosine_similarity(source_emb, target_emb, dim=-1).item()
+
+    return sim
+
+
+def get_keywords_sim(term1, term2):
+    with open("data/keywords.json", "r") as f:
+        data = json.load(f)
+
+    emb_model = SentenceTransformer("all-mpnet-base-v2", device="cuda:1")
+
+    keyword_emb1 = {}
+    for keyword in data[term1]:
+        keyword_emb1[keyword] = emb_model.encode(keyword, convert_to_tensor=True)
+
+    keyword_emb2 = {}
+    for keyword in data[term2]:
+        keyword_emb2[keyword] = emb_model.encode(keyword, convert_to_tensor=True)
+
+    corr = {}
+    for keyword1 in keyword_emb1:
+        corr[keyword1] = {}
+
+        for keyword2 in keyword_emb2:
+            corr[keyword1][keyword2] = torch.cosine_similarity(
+                keyword_emb1[keyword1], keyword_emb2[keyword2], dim=-1
+            ).item()
+
+    return corr
