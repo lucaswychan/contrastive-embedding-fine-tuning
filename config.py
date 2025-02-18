b3f9719af4eb46acb0214f9b4d29649bd118db40
LucasChan <chanconan@gmail.com>
2025-02-18 16:25:28 +0800

    add dataset name

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ DATA_DIR = "/home/data/hlibt/checklist2_WIP/"
 
 HF_cases_path = os.path.join(DATA_DIR, "HF_cache", "cases")
 HF_KBs_path = os.path.join(DATA_DIR, "HF_cache", "KBs")
+
+DATASET_NAME = "GDPR"
